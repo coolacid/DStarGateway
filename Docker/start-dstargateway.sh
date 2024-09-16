5e55637eea5d220f2d50cd7c73fcc0acefcd608f
Jason Kendall <jakendall@gmail.com>
2024-09-16 14:47:26 -0400

    More upstream changes

--- Docker/start-dstargateway.sh
+++ Docker/start-dstargateway.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-wget http://www.pistar.uk/downloads/DExtra_Hosts.txt -nv -O /usr/local/share/dstargateway.d/DExtra_Hosts.txt
-wget http://www.pistar.uk/downloads/DCS_Hosts.txt -nv -O /usr/local/share/dstargateway.d/DCS_Hosts.txt
-wget http://www.pistar.uk/downloads/DPlus_Hosts.txt -nv -O /usr/local/share/dstargateway.d/DPlus_Hosts.txt
-
 /usr/local/bin/dstargateway /usr/local/etc/dstargateway.d/dstargateway.cfg
